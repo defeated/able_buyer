16c9c7de2fec700b8e0491ea59febf1e3bc490b3
eddie cianci <defeated2k4@gmail.com>
2013-12-29 00:56:10 -0500

    refactor person's name into internal User::Name class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ActiveRecord::Base
   has_many :offers, dependent: :destroy
 
+  class Name
+    attr_reader :first, :last
+
+    def initialize(user)
+      @first, @last = user.first_name, user.last_name
+    end
+
+    def full
+      "#{ first } #{ last }"
+    end
+
+    def short
+      "#{ first } #{ last[0] }."
+    end
+  end
+
+  def name
+    @name ||= Name.new(self)
+  end
+
   def self.from_omniauth(auth)
     key   = auth.slice(:provider, :uid)
     attrs = auth[:info].slice(:email, :first_name, :last_name, :image)
